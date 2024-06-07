bbaa98f1e3dcf248f630da6a1d87d3d739add5a0
Ander-P <pradoanderson1203@outlook.com>
2024-06-07 10:40:32 -0500

    chore: udpate params of create user

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -4,61 +4,70 @@ import axios from "axios";
 import { getErrorCreateUser, getErrorsLoginUser } from "../helper";
 
 export const login = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
-    try {
-        const userQuery = await sdkAdmin.firestore()
-            .collection("Users")
-            .where("email", "==", email)
-            .limit(1)
-            .get();
+  const { email, password } = req.body;
+  try {
+    const userQuery = await sdkAdmin
+      .firestore()
+      .collection("Users")
+      .where("email", "==", email)
+      .limit(1)
+      .get();
 
-        const userData = userQuery.docs.map((doc) => {
-            const data = doc.data();
-            return { ...data, documentId: doc.id };
-        })
+    const userData = userQuery.docs.map((doc) => {
+      const data = doc.data();
+      return { ...data, documentId: doc.id };
+    });
 
-        if (userData.length === 0) throw "Unregistered user";
+    if (userData.length === 0) throw "Unregistered user";
 
-        const { data } = await axios.post(
-            `${process.env.BASE_URL_IG}/accounts:signInWithPassword?key=${process.env.FIREBASE_API_WEB}`,
-            {
-                email,
-                password,
-                returnSecureToken: true,
-            }
-        );
+    const { data } = await axios.post(
+      `${process.env.BASE_URL_IG}/accounts:signInWithPassword?key=${process.env.FIREBASE_API_WEB}`,
+      {
+        email,
+        password,
+        returnSecureToken: true,
+      }
+    );
 
-        res.status(200).json({  uid: data.localId, ...userData[0] })
-    } catch (error: any) {
-        console.log(error);
-        
-        const errorInfo = getErrorsLoginUser(error?.response?.data?.error?.message);
-        res.status(400).json({ msg: `${errorInfo || "An unexpected error occurred"}` });
-    }
-}
+    res.status(200).json({ uid: data.localId, ...userData[0] });
+  } catch (error: any) {
+    console.log(error);
+
+    const errorInfo = getErrorsLoginUser(error?.response?.data?.error?.message);
+    res
+      .status(400)
+      .json({ msg: `${errorInfo || "An unexpected error occurred"}` });
+  }
+};
 
 export const register = async (req: Request, res: Response) => {
-    try {
-        const { email, password, name } = req.body;
-        if (!(email && password && name)) return res.status(400).json({ msg: "Params not found" });
+  try {
+    const { email, password, userName, birthday } = req.body;
 
-        const newUser = await sdkAdmin.auth().createUser({
-            email,
-            password,
-        });
-
-        const uid = newUser.uid;
-
-        await sdkAdmin.firestore().collection("Users").add({
-            uid,
-            email,
-            createdAt: new Date(),
-            name
-        })
-
-        res.status(200).json({ msg: "User created successfully", uid })
-    } catch (error: any) {
-        console.log(error);
-        res.status(400).json({ msg: `${getErrorCreateUser(error?.errorInfo?.code ?? error)}` })
+    if (!(email && password && userName && birthday)) {
+      return res.status(400).json({ msg: "Params not found" });
     }
-}   
+
+    const newUser = await sdkAdmin.auth().createUser({
+      email,
+      password,
+    });
+
+    const uid = newUser.uid;
+
+    await sdkAdmin.firestore().collection("Users").add({
+      uid,
+      email,
+      createdAt: new Date(),
+      password,
+      userName,
+      birthday,
+    });
+
+    res.status(200).json({ msg: "User created successfully", uid });
+  } catch (error: any) {
+    res
+      .status(400)
+      .json({ msg: `${getErrorCreateUser(error?.errorInfo?.code ?? error)}` });
+  }
+};
